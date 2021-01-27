1db493f923369547a4cf1de0b1b9d2cd6bae1721
Kewun <Kewun@KEWUN-PC>
2021-01-27 21:34:40 +0100

    Fix nicknames sync

--- LUServer/LUServer.cpp
+++ LUServer/LUServer.cpp
@@ -72,7 +72,7 @@ void ParseData(ENetHost* server, int id, char* data)
 			client_map[id]->SetUsername(username);
 
 			break;
-		}
+		} 
 	}
 }
 
@@ -106,7 +106,7 @@ int main(int argc, char** argv)
 		printf("An error occurred while trying to create an ENet server host.");
 		return 1;
 	}
-
+	int new_player_id = 0;
 	// gameloop
 	while (1!=2)
 	{
@@ -115,27 +115,44 @@ int main(int argc, char** argv)
 		
 		while (enet_host_service(server, &event, 1000) > 0)
 		{ 
-			switch (event.type)
+			if (event.type == ENET_EVENT_TYPE_CONNECT)
 			{
-			case ENET_EVENT_TYPE_CONNECT:
 				printf("A new client connected from %x:%u.\n",
 					event.peer->address.host,
 					event.peer->address.port);
-				break;
 
-			case ENET_EVENT_TYPE_RECEIVE:
+				for (auto const& x : client_map)
+				{
+					char send_data[1024] = { '\0' };
+					sprintf(send_data, "2|%d|%s", x.first, x.second->GetUsername().c_str());
+					BroadcastPacket(server, send_data);
+				}
+
+				new_player_id++;
+				client_map[new_player_id] = new Clients(new_player_id);
+				event.peer->data = client_map[new_player_id];
+
+				char data_to_send[126] = { '\0' };
+				sprintf(data_to_send, "3|%d", new_player_id);
+				SendPacket(event.peer, data_to_send);
+			}
+			else if (event.type == ENET_EVENT_TYPE_RECEIVE)
+			{
 				printf("A packet of length %u containing %s was received from %s on channel %u.\n",
 					event.packet->dataLength,
 					event.packet->data,
 					event.peer->data,
 					event.channelID);
-				/* Clean up the packet now that we're done using it. */
+				char data[256];
+				sprintf(data, "%s", event.packet->data);
+				ParseData(server, static_cast<Clients*>(event.peer->data)->GetID(), data);
 				enet_packet_destroy(event.packet);
-				break;
-
-			case ENET_EVENT_TYPE_DISCONNECT:
+			}
+			else if (event.type== ENET_EVENT_TYPE_DISCONNECT){
 				printf("%s disconnected.\n", event.peer->data);
-				/* Reset the peer's client information. */
+				char disconnected_data[126] = { '\0' };
+				sprintf(disconnected_data, "4|%d", static_cast<Clients*>(event.peer->data)->GetID());
+				BroadcastPacket(server, disconnected_data);
 				event.peer->data = NULL;
 			}
 		}
