ad01345e134eaba51c6e5c0ff414ce90518567a2
Kewun <Kewun@KEWUN-PC>
2021-01-27 20:54:28 +0100

    Data Parser

--- LUServer/LUServer.cpp
+++ LUServer/LUServer.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <enet/enet.h>
+#include <map>
+
 
 #pragma comment(lib,"enet.lib")
 
@@ -12,23 +14,87 @@
 #pragma comment(lib,"ws2_32.lib")
 #pragma comment(lib,"winmm.lib") 
 
-ENetEvent event;
-ENetAddress address;
-ENetHost* server;
-
-
-
-int main()
+class Clients
 {
-	printf("Liberty Unleashed 0.1 Server started \n");
-    if (enet_initialize() != 0)
-    {
-        fprintf(stderr, "An error occurred while initializing ENet.\n");
-        return EXIT_FAILURE;
-    }
-    atexit(enet_deinitialize);
-    address.host = ENET_HOST_ANY;
-    address.port = 7777;
+private:
+	int m_id;
+	std::string m_username;
+
+public:
+	Clients(int id) : m_id(id) {}
+
+	void SetUsername(std::string username) { m_username = username; }
+
+	int GetID() { return m_id; }
+	std::string GetUsername() { return m_username; }
+};
+
+std::map<int, Clients*> client_map;
+
+void SendPacket(ENetPeer* peer, const char* data)
+{
+	ENetPacket* packet = enet_packet_create(data, strlen(data) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_peer_send(peer, 0, packet);
+}
+
+void BroadcastPacket(ENetHost* server, const char* data)
+{
+	ENetPacket* packet = enet_packet_create(data, strlen(data) + 1, ENET_PACKET_FLAG_RELIABLE);
+	enet_host_broadcast(server, 0, packet);
+}
+
+void ParseData(ENetHost* server, int id, char* data)
+{
+	int data_type;
+	sscanf(data, "%d|", &data_type);
+
+	switch (data_type)
+	{
+		case 1:
+		{
+			char msg[80];
+			sscanf(data, "%*d|%[^\n]", &msg);
+
+			char send_data[1024] = { '\0' };
+			sprintf(send_data, "1|%d|%s", id, msg);
+			BroadcastPacket(server, send_data);
+			break;
+		}
+		case 2:
+		{
+			char username[80];
+			sscanf(data, "2|%[^\n]", &username);
+
+			char send_data[1024] = { '\0' };
+			sprintf(send_data, "2|%d|%s", id, username);
+
+			BroadcastPacket(server, send_data);
+			client_map[id]->SetUsername(username);
+
+			break;
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	if (enet_initialize() != 0)
+	{
+		fprintf(stderr, "An error occurred while initializing ENet.\n");
+		return EXIT_FAILURE;
+	}
+	atexit(enet_deinitialize);
+
+	ENetEvent event;
+	ENetAddress address;
+	ENetHost* server;
+
+	address.host = ENET_HOST_ANY; 
+	
+	address.port = 7777;
+
+
+
 	server = enet_host_create(&address	/* the address to bind the server host to */,
 		32	/* allow up to 32 clients and/or outgoing connections */,
 		1	/* allow up to 1 channel to be used, 0. */,
@@ -41,11 +107,14 @@ int main()
 		return 1;
 	}
 
-	while (true)
+	// gameloop
+	while (1!=2)
 	{
 		ENetEvent event;
-		while (enet_host_service(server, &event, 50) > 0)
-		{
+		/* Wait up to 1000 milliseconds for an event. */
+		
+		while (enet_host_service(server, &event, 1000) > 0)
+		{ 
 			switch (event.type)
 			{
 			case ENET_EVENT_TYPE_CONNECT:
@@ -60,16 +129,20 @@ int main()
 					event.packet->data,
 					event.peer->data,
 					event.channelID);
+				/* Clean up the packet now that we're done using it. */
 				enet_packet_destroy(event.packet);
 				break;
 
 			case ENET_EVENT_TYPE_DISCONNECT:
 				printf("%s disconnected.\n", event.peer->data);
+				/* Reset the peer's client information. */
 				event.peer->data = NULL;
 			}
 		}
 	}
 
 	enet_host_destroy(server);
-	return 1;
+
+	return 0;
 }
+ 
