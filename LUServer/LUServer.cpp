193d6c1027112f0503156015a1e599a0f9f2bd3f
Kewun <Kewun@KEWUN-PC>
2021-01-27 20:23:54 +0100

    enet

--- LUServer/LUServer.cpp
+++ LUServer/LUServer.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <enet/enet.h>
+
+#pragma comment(lib,"enet.lib")
+
+#pragma warning(disable: 4018)
+#pragma warning(disable: 4244)
+#pragma warning(disable: 4996)
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"winmm.lib") 
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	printf("Liberty Unleashed 0.1 Server started \n");
+	while (1 != 2)
+	{
+		Sleep(1);
+	}
+	return 1;
 }
